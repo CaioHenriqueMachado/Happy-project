e9a03cd338a5c0799bc94695da934fb08a37a0bb
Caio <caio.student@outlook.com>
2020-10-13 23:53:02 -0300

    iniciando parte web

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/global.css';
+import './styles/landing.css';
+
+
+function App() {
+  return (
+    <div id="page-landing">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
