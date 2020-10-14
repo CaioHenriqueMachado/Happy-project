c80df6200afed1f687d4dd7c4218c2e492abc390
Caio <caio.student@outlook.com>
2020-10-14 00:28:00 -0300

    criand tela inicial

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 
 import './styles/global.css';
-import './styles/landing.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg';
+import landingImg from './images/landing.svg';
 
 
 function App() {
   return (
     <div id="page-landing">
-      <h1>Hello World</h1>
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve a felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Carapicuiba</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <a href="" className="enter-app">
+          (seta)
+        </a>
+
+      </div>
     </div>
   );
 }
