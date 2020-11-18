0205c0cba1e1bfa083864daa0403b6d18f262dca
Caio <caio.student@outlook.com>
2020-11-18 11:50:17 -0300

    ajustando mapa

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -91,9 +91,7 @@ export default function OrphanagesMap() {
               zoom={15}
               onclick={hanldeMapClick}
             >
-              <TileLayer 
-                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-              />
+              <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
               { position.latitude  != 0 && (
                 <Marker 
